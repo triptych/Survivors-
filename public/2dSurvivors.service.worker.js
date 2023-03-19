de5e9423bb47e3ee9303971673cad063f04a08f4
Andrew Wooldridge <triptych@gmail.com>
2023-03-19 18:29:36 +0000

    updating area, adding camera

--- public/2dSurvivors.service.worker.js
+++ public/2dSurvivors.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1679159165|46560124102";
+const CACHE_VERSION = "1679250484|137881265032";
 const CACHE_PREFIX = "2dSurvivors-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "2dSurvivors.offline.html";
--- public_old/godot4test001.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-title "godot4test001"
-"%~dp0godot4test001.exe" "%*"
-pause > nul
